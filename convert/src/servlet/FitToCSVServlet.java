8524ae9acc809e8dd6d11da9cbf8c5fa51194e22
jkujku <j.kummerloe@gmail.com>
2016-03-17 15:00:19 +0100

    added java project

---
+++ convert/src/servlet/FitToCSVServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FitToCSVServlet extends HttpServlet implements Servlet {
+
+	private static final long serialVersionUID = 462069270850827125L;
+
+	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String result = "Hello World";
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/plain");
+		response.getWriter().write(result);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)	throws IOException {
+		processRequest(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		processRequest(request, response);
+	}
+
+}
